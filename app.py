a63cbc26f29e7322400ecda8ebe94e206ca1e115
Renata Feregrino <renata.mesinas@ciencias.unam.mx>
2025-07-21 02:34:18 -0600

    correccion login y formulario, hasta menu

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
+
 
 app =  Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///usuarios.db'
@@ -8,34 +9,59 @@ db = SQLAlchemy(app)
 
 class Usuario(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    nombre = db.Column(db.String(50), nullable=False)
+    nombre = db.Column(db.String(50), nullable=False, unique=True)
+    contrasena= db.Column(db.String(50), nullable=False)
     puntaje = db.Column(db.Integer, default=0)
+
+    #atributos para las preguntas
+    agua = db.Column(db.Float, default=0)
+    luz = db.Column(db.Float, default=0)
+
+
+
 
 with app.app_context():
     db.create_all()
 
-#primera página
-@app.route("/") #decorador de FLASK: lit dice qué hacer (o mostrar) cuando se está en esa url
-def index():
-    return render_template("index.html") #función de FLASK: busca el archivo html en la carpeta templates. Así que por el amor de Dios, pon todo en templates
-
-@app.route("/inicio", methods=['POST']) #methods, decorador de FLASK: dice qué tipo de solicitud acepta la url
-def inicio():
-    nombre = request.form["nombre"] #request.form[]: guarda los datos enviados de un formulario (tipo diccionario, busca la key, debe coincidir con lo que esté en name)
-    nuevo_usuario = Usuario(nombre=nombre)
-    db.session.add(nuevo_usuario) 
-    db.session.commit() #guarda al usuario
-    return redirect(f"/p1/{nuevo_usuario.id}") #manejamos por id
-
-@app.route("/p1/<int:id_usuario>", methods=["GET", "POST"]) #pasamos el usuario de una página a otra
-def p1(id_usuario):
-    usuario = Usuario.query.get_or_404(id_usuario)
-    #usuario = Usuario.query.get(id_usuario)
-    #if usuario is None:
-        #abort(404)
+@app.route("/registro", methods=["GET", "POST"])  #methods, decorador de FLASK: dice qué tipo de solicitud acepta la url
+def registro():
     if request.method == "POST":
-        p1 = int(request.form.get("p1", 0))
-        usuario.puntaje += p1
+        nombre = request.form["nombre"] #request.form[]: guarda los datos enviados de un formulario (tipo diccionario, busca la key, debe coincidir con lo que esté en name)
+        contrasena = request.form["contrasena"]
+        nuevoUsuario = Usuario(nombre=nombre, contrasena=contrasena)
+        db.session.add(nuevoUsuario)
+        db.session.commit()
+        return redirect(url_for("login"))
+    return render_template("registro.html")
+
+@app.route("/", methods=["GET", "POST"]) #decorador de FLASK: lit dice qué hacer (o mostrar) cuando se está en esa url
+def login():
+    if request.method == "POST":
+        nombre = request.form["nombre"]
+        contrasena = request.form["contrasena"]
+        usuario = Usuario.query.filter_by(nombre=nombre, contrasena=contrasena).first() #.query : sistema de consultas
+
+        if usuario:
+            return redirect(f"/menu/{usuario.id}")
+        else:
+            error = "Usuario o contraseña incorrectos"
+            return render_template("login.html", error=error) 
+    return render_template("login.html")
+
+@app.route("/menu/<int:id_usuario>", ) #pasamos el usuario de una página a otra
+def menu(id_usuario):
+    usuario = Usuario.query.get_or_404(id_usuario)
+    return render_template("menu.html", usuario=usuario)
+
+
+
+#PREGUNTAS 
+@app.route("/p1/<int:id_usuario>", methods=["GET", "POST"])
+def p1(id_usuario):
+    usuario = Usuario.query.get_or_404(id_usuario) #busca en la base de datos con ese id, si no lo encuentra manda error
+    if request.method == "POST":
+        agua = float(request.form.get("agua", 0))
+        usuario.agua = agua
         db.session.commit()
         return redirect(f"/p2/{usuario.id}")
     return render_template("pregunta1.html", usuario=usuario) # usuario (var del html) = usuario (var de Python (obtenida del request.form)), pasa como param
@@ -44,21 +70,15 @@ def p1(id_usuario):
 def p2(id_usuario):
     usuario = Usuario.query.get_or_404(id_usuario)
     if request.method == "POST":
-        p2 = int(request.form.get("p2", 0))
-        usuario.puntaje += p2
+        luz = float(request.form.get("luz", 0))
+        usuario.luz = luz
         db.session.commit()
-        return redirect(f"/p3/{usuario.id}")
+        return redirect(f"/p3{usuario.id}")
     return render_template("pregunta2.html", usuario=usuario)
 
-@app.route("/p3/<int:id_usuario>", methods=["GET", "POST"])
-def p3(id_usuario):
-    usuario = Usuario.query.get_or_404(id_usuario)
-    if request.method == "POST":
-        p3 = int(request.form.get("p3", 0))
-        usuario.puntaje += p3
-        db.session.commit()
-        return f"Gracias {usuario.nombre}. Tu puntaje final es: {usuario.puntaje}"
-    return render_template("pregunta3.html", usuario=usuario)
+
+
+
 
 
 
